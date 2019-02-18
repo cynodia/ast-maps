6dde95b460b978ce1ec7b3945d481b81b1bbd2e0
Andreas Tønnesen <andreas.tonnesen@otrum.com>
2019-02-18 16:13:36 +0100

    Bah... openlayer is not really that intuitive...

--- index.js
+++ index.js
@@ -9,94 +9,106 @@ import {fromLonLat} from 'ol/proj';
 
 const raster = new TileLayer({
     source: new BingMaps({
-        imagerySet: 'Aerial',
+        imagerySet: 'AerialWithLabels',
         key: 'AnYziXEovRK4etjl5i7WUDuZjMlhkL6QFxqEh9Yl2_S24mltSUdmJMGyUNwkPci9'
     })
 });
 
-const vector1 = new VectorLayer({
-    source: new VectorSource({
-        url: 'http://localhost:1234/data/trails/tungvekter/test1.gpx',
-        format: new GPX()
-    }),
-    style: function(feature) {
-        console.log(feature.getGeometry().getType());
-        switch(feature.getGeometry().getType()) {
-            case 'Point':
-                return new Style({
-                    image: new CircleStyle({
-                        fill: new Fill({
-                            color: 'rgba(255,255,0,0.4)'
-                        }),
-                        radius: 5,
-                        stroke: new Stroke({
-                            color: '#ff0',
-                            width: 1
-                        })
-                    })
-                });
-            case 'LineString':
-                return new Style({
-                    stroke: new Stroke({
-                        color: '#f00',
-                        width: 3
-                    })
-                });
-            case 'MultiLineString':
-                return new Style({
-                    stroke: new Stroke({
-                        color: '#0f0',
-                        width: 3
-                    })
-                });
-        }
+const trails = [
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/halvfire.gpx',
+        title: 'Halv Fire',
+        color: '#000000'
+    },
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/kunto.gpx',
+        title: 'Kun To',
+        color: '#00ff00'
+    },
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/hundetoppen.gpx',
+        title: 'Hundetoppen',
+        color: '#f0e000'
+    },
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/vestbredden.gpx',
+        title: 'Vestbredden',
+        color: '#a47700'
+    },
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/ostbredden.gpx',
+        title: 'Ostbredden',
+        color: '#a47700'
+    },
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/sworks.gpx',
+        title: 'S-Works',
+        color: '#00a477'
+    },
+    {
+        url: 'http://localhost:1234/data/trails/tungvekter/xkjerringene.gpx',
+        title: 'X-Kjærringene',
+        color: '#0a4770'
     }
-});
+];
 
-const vector2 = new VectorLayer({
-    source: new VectorSource({
-        url: 'http://localhost:1234/data/trails/tungvekter/test2.gpx',
-        format: new GPX()
-    }),
-    style: function(feature) {
-        console.log(feature.getGeometry().getType());
-        switch(feature.getGeometry().getType()) {
-            case 'Point':
-                return new Style({
-                    image: new CircleStyle({
-                        fill: new Fill({
-                            color: 'rgba(255,255,0,0.4)'
-                        }),
-                        radius: 5,
-                        stroke: new Stroke({
-                            color: '#ff0',
-                            width: 1
+function buildVector(item) {
+    return new VectorLayer({
+        source: new VectorSource({
+            url: item.url,
+            format: new GPX(),
+            attributions: item.title
+        }),
+        style: function (feature) {
+            console.log(feature.getGeometry().getType());
+            switch (feature.getGeometry().getType()) {
+                case 'Point':
+                    return new Style({
+                        image: new CircleStyle({
+                            fill: new Fill({
+                                color: 'rgba(255,255,0,0.4)'
+                            }),
+                            radius: 6,
+                            stroke: new Stroke({
+                                color: '#ffff00',
+                                width: 3
+                            })
                         })
-                    })
-                });
-            case 'LineString':
-                return new Style({
-                    stroke: new Stroke({
-                        color: '#f00',
-                        width: 3
-                    })
-                });
-            case 'MultiLineString':
-                return new Style({
-                    stroke: new Stroke({
-                        color: '#00f',
-                        width: 3
-                    })
-                });
+                    });
+                case 'LineString':
+                    return new Style({
+                        stroke: new Stroke({
+                            color: '#f00',
+                            width: 3
+                        })
+                    });
+                case 'MultiLineString':
+                    return new Style({
+                        stroke: new Stroke({
+                            color: item.color,
+                            width: 3
+                        })
+                    });
+            }
         }
-    }
-});
+    });
+}
+
+let layers = [ raster ];
+let vectors = [];
+
+for(let i = 0; i < trails.length; i++) {
+    const item = buildVector(trails[i]);
+    item.set("title", trails[i].title);
+    layers.push(item);
+    vectors.push(item);
+}
 
 const map = new Map({
-    layers: [raster, vector1, vector2],
+    layers: layers,
     target: document.getElementById('map'),
     view: new View({
-        center: fromLonLat([8.785291, 58.483912]),
+        center: fromLonLat([8.785800, 58.483912]),
         zoom: 15
     })
 });
@@ -110,7 +122,7 @@ const displayFeatureInfo = function(pixel) {
         const info = [];
         let i, ii;
         for (i = 0, ii = features.length; i < ii; ++i) {
-            info.push(features[i].get('desc'));
+            info.push(features[i].getProperties().name);
         }
         document.getElementById('info').innerHTML = info.join(', ') || '(unknown)';
         map.getTarget().style.cursor = 'pointer';
