6b85a69a11102f6d6c8c2a2219f172b63952d0dd
Andreas Tønnesen <andreas.tonnesen@otrum.com>
2019-02-27 03:16:13 +0100

    geolocation

--- index.js
+++ index.js
@@ -137,8 +137,21 @@ function mapUserLocation() {
     }
 }
 
-function geolocationFail(e) {
-    alert('Can\'t retrieve position.\nError: ' + e.code);
+function geolocationFail(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        default:
+            alert("An unknown error occurred.");
+            break;
+    }
 }
 
 function updatePosition(pos) {
