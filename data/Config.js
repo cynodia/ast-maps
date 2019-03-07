debba1dfb54030ab29e51a00236b8424e8e714c8
Andreas Tønnesen <andreas.tonnesen@otrum.com>
2019-03-07 11:30:13 +0100

    More trails

--- data/Config.js
+++ data/Config.js
@@ -436,6 +436,26 @@ const mmConfigurations = {
                 images: {
                     trailStart: 'image5.jpeg'
                 }
+            },
+            {
+                url: 'data/trails/tungvekter/motsaeden.gpx',
+                title: 'Opp mot Sæden',
+                level: 1,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
+            },
+            {
+                url: 'data/trails/tungvekter/nedfraribberaasen.gpx',
+                title: 'Ribberåsen ned',
+                level: 2,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
             }
         ]
     },
