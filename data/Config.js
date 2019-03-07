1738e4eae4560e2071251d7bbffd8c1ed435d7c2
Andreas Tønnesen <andreas.tonnesen@otrum.com>
2019-03-07 08:05:21 +0100

    Some new trails

--- data/Config.js
+++ data/Config.js
@@ -366,6 +366,56 @@ const mmConfigurations = {
                 images: {
                     trailStart: 'image5.jpeg'
                 }
+            },
+            {
+                url: 'data/trails/tungvekter/toppentildiagonalen.gpx',
+                title: 'Toppen til diagonalen',
+                level: 2,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
+            },
+            {
+                url: 'data/trails/tungvekter/hftilskauen.gpx',
+                title: 'Til skauen',
+                level: 1,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
+            },
+            {
+                url: 'data/trails/tungvekter/skauenut.gpx',
+                title: 'Ut fra skauen',
+                level: 1,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
+            },
+            {
+                url: 'data/trails/tungvekter/kamelen.gpx',
+                title: 'Kamelen',
+                level: 3,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
+            },
+            {
+                url: 'data/trails/tungvekter/walltillangemyr.gpx',
+                title: 'Wall til Langemyr',
+                level: 1,
+                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
+                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                images: {
+                    trailStart: 'image5.jpeg'
+                }
             }
         ]
     },
