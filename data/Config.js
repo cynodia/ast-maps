4b32cb244cac0bc17cfd3fe20384653fea19d75b
Andreas Tønnesen <andreas.tonnesen@otrum.com>
2019-03-18 19:29:30 +0100

    Fixxes

--- data/Config.js
+++ data/Config.js
@@ -269,7 +269,7 @@ const mmConfigurations = {
             },
             {
                 url: 'data/trails/tungvekter/langsveien.gpx',
-                title: 'Langs veien',
+                title: 'Heitjenn',
                 level: 2,
                 findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
                 infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -439,7 +439,7 @@ const mmConfigurations = {
             },
             {
                 url: 'data/trails/tungvekter/philip.gpx',
-                title: 'Philip',
+                title: 'Fillip',
                 level: 2,
                 findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
                 infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -449,18 +449,8 @@ const mmConfigurations = {
             },
             {
                 url: 'data/trails/tungvekter/langslangemyr.gpx',
-                title: 'Ørretbekken',
+                title: 'Ørretdammen',
                 level: 2,
-                findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
-                infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                images: {
-                    trailStart: 'image5.jpeg'
-                }
-            },
-            {
-                url: 'data/trails/tungvekter/parkeringsrunden.gpx',
-                title: 'Parkeringsrunden',
-                level: 1,
                 findStartText: "Tekstlig informasjon om hvor en finner startpunkt samt bilde(r) av inngang til sti.",
                 infoText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 images: {
